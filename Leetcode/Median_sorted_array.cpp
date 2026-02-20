ce1904b28cd01aa2a6ec70ee6967cb3a411818d8
Soham-0047 <solutionnoton@gmail.com>
2026-02-20 22:49:17 +0530

    feat: merge

--- Leetcode/Median_sorted_array.cpp
+++ Leetcode/Median_sorted_array.cpp
@@ -69,6 +69,29 @@ void mergeSort(vector<int>&v, int l, int r){
 
 }
 
+double findMedianTwoSortedArray(vector<int>&v1,vector<int>&v2){
+    int n1 =  v1.size();
+    int n2 = v2.size();
+    int i=0, j=0;
+    vector<int>merge;
+    while(i<n1 && j <n2){
+        if(v1[i]<v2[j]) merge.push_back(v1[i++]);
+        else merge.push_back(v2[j++]);
+    }
+
+    // For remainignelements 
+    while(i<n1) merge.push_back(v1[i++]);
+    while(j<n2) merge.push_back(v2[j++]);
+
+    int n = merge.size();
+    // printList(merge);
+    if(n%2 == 1){
+        return merge[n/2];
+    } else {
+        return merge[n/2-1] + merge[n/2] / 2.0;
+    }
+}
+
 int main(){
 
     // vector<int>v = {44,223,556,22,909,123,90};
@@ -78,9 +101,13 @@ int main(){
     vector<int>v1 = {34,56,12,4,20,45};
     vector<int>v2 = {8,55,89,90};
     int l = 0;
-    int r = v1.size()-1;
-    mergeSort(v1,l,r);
-    printList(v1);
-
+    int r1 = v1.size()-1;
+    int r2 = v2.size()-1;
+    mergeSort(v1,l,r1);
+    mergeSort(v2,l,r2);
+    // printList(v1);
+    // Find median of two sorted array
+    double md = findMedianTwoSortedArray(v1,v2);
+    cout<<md<<" "<<endl;
     return 0;
 }
