691d632d6f7af76cb091b6b7696c071ebcf7e53a
Soham-0047 <solutionnoton@gmail.com>
2026-04-03 13:51:50 +0530

    feat

--- Leetcode/1-4-26.cpp
+++ Leetcode/1-4-26.cpp
@@ -2,16 +2,32 @@
 #include<vector>
 using namespace std;
 
+// 1,2,3 -> 1,2,3, []
 
-
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> curr;
-
-        solve(0, nums, curr, ans);
-        return ans;
+void recursiveSubset(vector<int>&v,vector<int>ans,int i){
+    int n = v.size();
+    if(i==n){
+        if(ans.size() < 1){
+            cout<<"[]"<<endl;
+        }
+        for(auto j:ans){
+            cout<<j<<" ";
+        }
+        cout<<" || Size"<<ans.size()<<"|| "<<endl;
+        return;
     }
-};
+
+    ans.push_back(v[i]);
+    recursiveSubset(v,ans,i+1);
+    ans.pop_back();
+    recursiveSubset(v,ans,i+1);
+
+}
+
+// Subset 2
+void recursiveSubset2(vector<int>&v,vector<int>&ans,int i){
+    int j 
+}
 
 void printList(vector<vector<int>>&vec){
     for(auto i : vec){
@@ -24,9 +40,15 @@ int main(){
     // 0001 ->    1 x 2^0 +  0 x 2^1 + 0 X 2^2 ...
     // 0010
     vector<vector<int>>vec;
+    // For example currently we have array
+    vector<int>arr = {1,2,3};
 
-    cout<<(1<<2)<<endl;
-    printList(vec);
+    // cout<<(1<<2)<<endl;
+    // printList(vec);
+
+    vector<int>ans;
+    recursiveSubset(arr,ans,0);
+
 
     return 0;
 }
