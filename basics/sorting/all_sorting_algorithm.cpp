ac6b4aed1efc5881f5c48f2a3582d5f3d67a03d6
Soham-0047 <solutionnoton@gmail.com>
2026-04-11 23:41:24 +0530

    feat: insertion sort

--- basics/sorting/all_sorting_algorithm.cpp
+++ basics/sorting/all_sorting_algorithm.cpp
@@ -20,6 +20,18 @@ void bubbleSort(vector<int>&v){
 
 // Insertion Sort
 
+void insertion_sort(vector<int>&v){
+    int n = v.size();
+    for(int i=1; i<n;i++){
+        int k = v[i];  //key value
+        int j = i-1;  //previous index
+        while(j>=0 && v[j] > k){
+            v[j+1] = v[j];
+            j--;
+        }
+        v[j+1] =k;
+    }
+}
 
 void vp(vector<int>&v)
 {
@@ -31,7 +43,8 @@ void vp(vector<int>&v)
 int main(){
 
     vector<int> v = {64,7,70,12,90,3};
-    bubbleSort(v);
+    // bubbleSort(v);
+    // insertion_sort(v);
     vp(v);
     return 0;
 }
