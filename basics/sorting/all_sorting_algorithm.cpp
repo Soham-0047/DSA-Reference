e6ced5aaec3d2d204b20e396e0bb77140609e676
Soham-0047 <solutionnoton@gmail.com>
2026-04-13 20:28:21 +0530

    feat: quick sort

--- basics/sorting/all_sorting_algorithm.cpp
+++ basics/sorting/all_sorting_algorithm.cpp
@@ -48,7 +48,71 @@ void selection_sort(vector<int>&v){
 }
 
 
-// 
+// Merege Sort
+// T.C:  O(nlogn) and space complexity: O(n)
+void merge(vector<int>&v,int l,int m,int r){
+
+    int n1 = m-l+1;
+    int n2 = r-m;
+
+    int *a = new int[n1];
+    int *b = new int[n2];
+
+    for(int i=0;i<n1;i++){
+        a[i] = v[l+i];
+    }
+    for(int i=0;i<n2;i++){
+        b[i] = v[m+1+i];
+    }
+
+    int i=0;
+    int j=0;
+    int k=l;
+
+    
+    while(i<n1 && j<n2){
+        if(a[i] <= b[j]){
+            v[k]= a[i];
+            i++;
+        }else{
+            v[k] = b[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i<n1){
+        v[k] = a[i];
+        i++;
+        k++;
+    }
+
+    while(j<n2){
+        v[k] = b[j];
+        j++;
+        k++;
+    }
+
+    delete[] a;
+    delete[] b;
+
+}
+
+void mergeSort(vector<int>&v,int l, int r){
+    if(l>=r) return;
+
+    int mid = l + (r-l)/2;
+
+    mergeSort(v,l,mid);
+    mergeSort(v,mid+1,r);
+    merge(v,l,mid,r);
+}
+
+
+// Quick sort
+
+
+// print the list
 void vp(vector<int>&v)
 {
     for(int i: v){
@@ -59,9 +123,11 @@ void vp(vector<int>&v)
 int main(){
 
     vector<int> v = {64,7,70,12,90,3};
+    int n = v.size();
     // bubbleSort(v);
     // insertion_sort(v);
-    selection_sort(v);
+    // mergeSort(v,0,n-1);
+
     vp(v);
     return 0;
 }
